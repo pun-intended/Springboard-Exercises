707e63ac7bdbf3b41e758db2ab245523c1694019
pun-intended <31040416+pun-intended@users.noreply.github.com>
2023-04-29 15:21:25 +0900

    Complete Exercises
    All exercise tasks completed and tested.

--- js-array-some-every/some-every.js
+++ js-array-some-every/some-every.js
@@ -1,45 +1,76 @@
 /*
-Write a function called hasOddNumber which accepts an array and returns true if the array contains at least one odd number, otherwise it returns false.
+Write a function called hasOddNumber which accepts an array and returns true if the 
+array contains at least one odd number, otherwise it returns false.
 
 Examples:
     hasOddNumber([1,2,2,2,2,2,4]) // true
     hasOddNumber([2,2,2,2,2,4]) // false
 */
 
-function hasOddNumber(arr) {}
+function hasOddNumber(arr) {
+    return arr.some(function(val) {
+        return val % 2 == 1;
+    })
+}
 
 /*
-Write a function called hasAZero which accepts a number and returns true if that number contains at least one zero. Otherwise, the function should return false
-
+Write a function called hasAZero which accepts a number and returns true if that 
+number contains at least one zero. Otherwise, the function should return false
 Examples:
     hasAZero(3332123213101232321) // true
     hasAZero(1212121) // false
 */
 
-function hasAZero(num) {}
+function hasAZero(num) {
+    numArray = Array.from(num.toString());
+    return numArray.some(function(item){
+        return item == 0;
+    })
+    
+}
 
 /*
-Write a function called hasOnlyOddNumbers which accepts an array and returns true if every single number in the array is odd. If any of the values in the array are not odd, the function should return false. 
+Write a function called hasOnlyOddNumbers which accepts an array and returns true 
+if every single number in the array is odd. If any of the values in the array are 
+not odd, the function should return false. 
 
 Examples:
     hasOnlyOddNumbers([1,3,5,7]) // true
     hasOnlyOddNumbers([1,2,3,5,7]) // false
 */
 
-function hasOnlyOddNumbers(arr) {}
+function hasOnlyOddNumbers(arr) {
+    return arr.every(function(num){
+        return num % 2 != 0;
+    })
+}
 
 /*
-Write a function called hasNoDuplicates which accepts an array and returns true if there are no duplicate values (more than one element in the array that has the same value as another). If there are any duplicates, the function should return false.
+Write a function called hasNoDuplicates which accepts an array and returns true if 
+there are no duplicate values (more than one element in the array that has the same 
+    value as another). If there are any duplicates, the function should return false.
 
 Examples:
     hasNoDuplicates([1,2,3,1]) // false
     hasNoDuplicates([1,2,3]) // true
 */
 
-function hasNoDuplicates(arr) {}
+function hasNoDuplicates(arr) {
+    inclArray = [];
+    return arr.every(function(num){
+        if(inclArray.includes(num)){
+            return false;
+         } else {
+            inclArray.push(num)
+            return true;
+        }       
+    })
+}
 
 /*
-Write a function called hasCertainKey which accepts an array of objects and a key, and returns true if every single object in the array contains that key. Otherwise it should return false.
+Write a function called hasCertainKey which accepts an array of objects and a key, 
+and returns true if every single object in the array contains that key. Otherwise 
+it should return false.
 
 Examples:
     var arr = [
@@ -53,10 +84,16 @@ Examples:
     hasCertainKey(arr,'isCatOwner') // false
 */
 
-function hasCertainKey(arr, key) {}
+function hasCertainKey(arr, key) {
+    return arr.every(function(val){
+        return val.hasOwnProperty(key);
+    })
+}
 
 /*
-Write a function called hasCertainValue which accepts an array of objects and a key, and a value, and returns true if every single object in the array contains that value for the specific key. Otherwise it should return false.
+Write a function called hasCertainValue which accepts an array of objects and a key, 
+and a value, and returns true if every single object in the array contains that value 
+for the specific key. Otherwise it should return false.
 
 Examples:
     var arr = [
@@ -71,4 +108,10 @@ Examples:
     
 */
 
-function hasCertainValue(arr, key, searchValue) {}
+function hasCertainValue(arr, key, searchValue) {
+    return arr.every(function(val) {
+        if (val.hasOwnProperty(key)){
+            return val[key] == searchValue;
+        } 
+    })
+}
