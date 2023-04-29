952de392ce265aa74c395b4afaea57c1a4591e9e
pun-intended <31040416+pun-intended@users.noreply.github.com>
2023-04-29 14:46:09 +0900

    Initial commit

---
+++ JS-todo.js
@@ -0,0 +1,41 @@
+/*
+Add a new todo (by submitting a form)
+Mark a todo as completed (cross out the text of the todo)
+Remove a todo
+
+Now that you have a functioning todo app, save your 
+todos in localStorage! Make sure that when the page
+refreshes, the todos on the page remain there.
+*/
+
+
+//create .css style sheet
+//create HTML page
+// tie Css, js, html page together
+
+/*
+add new todo
+ - create form input
+ - turn form input into list item in unordered list
+ - add remove button
+ - add checkboxes to toggle strikeout
+*/
+
+/*
+Mark todo as complete
+- auto attach button and checkbox to item
+- apply checkbox and button event handlers to UL
+- checkbox toggles strikethrough
+*/
+
+/*
+Remove Item
+- remove button removes target list item
+*/
+
+/*
+Save todos in localStorage
+- JSON.stringify() ul?
+- how to store HTML block in localStorage
+*/
+
