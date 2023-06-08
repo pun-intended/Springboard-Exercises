d1553c6a2b5d2171567b5201b07dba8e89c4ac8a
pun-intended <31040416+pun-intended@users.noreply.github.com>
2023-06-08 20:12:21 +0900

    Complete Exercises

--- python-oo-practice/serial.py
+++ python-oo-practice/serial.py
@@ -20,3 +20,18 @@ class SerialGenerator:
     100
     """
 
+    def __init__(self, start=1):
+        """Create an instance of the serial generator.  Start at 1 if no start value given"""
+        self.start = start
+        self.counter = 0
+    
+    def generate(self):
+        """Return the next number in the serial"""
+        num = self.start + self.counter
+        self.counter += 1
+        return num
+        
+    def reset(self):
+        """Reset the serial to the initial start value"""
+        self.counter = 0
+
--- python-oo-practice/wordfinder.py
+++ python-oo-practice/wordfinder.py
@@ -1,5 +1,18 @@
+from random import choice
 """Word Finder: finds random words from a dictionary."""
 
 
 class WordFinder:
-    ...
+    def __init__(self, filename=None):
+        """Create a WordFinder instance, populate the wordlist if a filename is provided."""
+        if filename:
+            self.add_words(filename)
+
+    def random(self):
+        """Return a random word from the word list"""
+        return choice(self.word_list)
+        
+    def add_words(self, filename):
+        """Add words to the word list from a given file"""
+        with open(filename) as f:
+            self.word_list = [line.strip() for line in f.readlines()]
