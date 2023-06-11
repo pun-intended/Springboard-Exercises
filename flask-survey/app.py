04387c0199d0dc89ccc967c4b99cbaf2a79dd999
pun-intended <31040416+pun-intended@users.noreply.github.com>
2023-06-11 12:54:03 +0900

    Add thank you message page, redirects, error handling

--- flask-survey/app.py
+++ flask-survey/app.py
@@ -1,7 +1,8 @@
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, redirect, flash
 import surveys
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = "thegooseisloose"
 
 responses = []
 
@@ -9,7 +10,39 @@ responses = []
 def get_home():
     return render_template("base.html")
 
-@app.route('/questions/<num>')
+@app.route('/questions/<int:num>')
 def get_question(num):
-    question = surveys.satisfaction_survey.questions[num]
-    return render_template("question.html", question = question)
+    if (num != len(responses)):
+        return redirect(f'/invalid/{num}')
+    if (len(responses) >= len(surveys.satisfaction_survey.questions)):
+        return redirect("/thankyou")
+    else:
+        question = surveys.satisfaction_survey.questions[num]
+        return render_template("question.html", question = question)
+
+@app.route('/answer', methods=["POST"])
+def add_answer():
+    ans = request.form['ans']
+    responses.append(ans)
+    num = len(responses)
+    return redirect(f"/questions/{num}")
+
+@app.route('/questions/')
+def go_to_question():
+    num = len(responses)
+    return redirect(f'/questions/{num}')
+
+@app.route('/invalid/<int:num>')
+def invalid_redirect(num):
+    if (num > len(responses)):
+        msg = "Please complete the survery questions in order"
+    elif (num < len(responses)):
+        msg = "This response has already been completed"
+    else:
+        msg = "This is not a valid question page"
+    flash(msg)
+    return redirect('/questions/')
+
+@app.route('/thankyou')
+def thank_you():
+    return render_template("thankyou.html")
