0cb2a29c083fb9ef16df7a7829cdf9fed846df07
pun-intended <31040416+pun-intended@users.noreply.github.com>
2023-11-19 14:39:03 +0900

    Slugify company code, Fix update syntax

--- express-biztime/routes/companies.js
+++ express-biztime/routes/companies.js
@@ -1,57 +1,61 @@
-const express = require("express");
+const express = require('express');
+const slugify = require('slugify')
 const router = new express.Router();
 
 const db = require('../db');
 const ExpressError = require("../expressError");
 
-router.get('/', async (req, res) => {
+router.get('/', async (req, res, next) => {
     try{
         const result = await db.query(
             "SELECT code, name FROM companies"
         )
-        return res.json({companies: results.rows})
+        return res.json({companies: result.rows})
     } catch (err) {
         return next(err)
     }
 })
 
-router.get('/:code', async (req, res) => {
-    const code = req.params
+router.get('/:code', async (req, res, next) => {
+    // const code = req.params
+    console.log(req.params.code)
     try{
         const result = await db.query(
-            `SELECT code, name, description FROM companies WHERE code=$1`
-            [code]
-        )
+            `SELECT code, name, description FROM companies WHERE code = $1`, 
+            [req.params.code]
+        );
+        console.log(result)
         if (result.rows.length === 0){
             throw new ExpressError(`No company found with code ${code}`, 404)
-        }
-        return res.json({company: result.rows[0]})
+        };
+        return res.json({company: result.rows[0]});
     } catch (err) {
         return next(err)
     }
 })
-router.post('/', async (req, res) => {
+router.post('/', async (req, res, next) => {
     const {code, name, description} = req.body
+    const slugCode = slugify(code, {remove: /[*+~.()'"!:@]/g, lower: true})
     try{
         const result = await db.query(
             `INSERT INTO companies(code, name, description)
             VALUES $1, $2, $3
-            RETURNING code, name, description`
-            [code, name, description]
+            RETURNING code, name, description`,
+            [slugCode, name, description]
         )
         return res.json({company: results.rows[0]})
     } catch (err) {
         return next(err)
     }
 })
-router.put('/:code', async (req, res) => {
+router.put('/:code', async (req, res, next) => {
     const code = req.params
     const {name, description} = req.body
     try{
         const result = await db.query(
-            `UPDATE companies SET $1, $2 
+            `UPDATE companies SET name=$1, description=$2 
             WHERE code = $3
-            RETURNING code, name, description`
+            RETURNING code, name, description`,
             [name, description, code]
         )
         if (result.rows.length === 0){
@@ -62,12 +66,12 @@ router.put('/:code', async (req, res) => {
         return next(err)
     }
 })
-router.delete('/:code', async (req, res) => {
+router.delete('/:code', async (req, res, next) => {
     const code = req.params
     try{
         const result = await db.query(
             `DELETE FROM companies
-            WHERE code = $1`
+            WHERE code = $1`,
             [code]
         )
         if (result.rows.length === 0){
