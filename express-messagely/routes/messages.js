c401af8e0c4b4b7bb1bfe983a19b593678ac5026
pun-intended <31040416+pun-intended@users.noreply.github.com>
2023-11-23 12:32:30 +0900

    Write message routes
    Write routes for handling get, post, and read requests

--- express-messagely/routes/messages.js
+++ express-messagely/routes/messages.js
@@ -1,3 +1,12 @@
+const express = require('express')
+const router = new express.Router()
+const ExpressError = require('../expressError')
+const Message = require('../models/message')
+const jwt = require('jsonwebtoken') // not needed?
+
+const { ensureLoggedIn, ensureCorrectUser } = require('../middleware/auth')
+
+const {SECRET_KEY} = require(`../config`)
 /** GET /:id - get detail of message.
  *
  * => {message: {id,
@@ -11,6 +20,26 @@
  *
  **/
 
+// NOTE: I feel I should be using ensureCorrectUser, but that requires the 
+// username to be in req.params, which they are not
+
+router.get('/:id', async (req, res, next) => {
+    try{
+        if(req.user){
+            const msg = await Message.get(req.params.id)
+            if (req.user == msg.from_user || req.user == msg.to_user){
+                return msg
+            } else {
+                return next(new ExpressError("Invalid permissions", 401))
+            }
+        } else{
+            return next(new ExpressError("Must be logged in to view message", 400))
+        }
+    } catch (e) {
+        return next(e)
+    }
+})
+
 
 /** POST / - post message.
  *
@@ -18,6 +47,22 @@
  *   {message: {id, from_username, to_username, body, sent_at}}
  *
  **/
+router.post('/', ensureLoggedIn, async (req, res, next) => {
+    try{
+        const msg = Message.create(
+            req.body.id, 
+            req.user, 
+            req.body.to_username, 
+            req.body.body,
+            Date.now()
+        )
+        return res.json({message: msg})
+    } catch (e) {
+        return next(e)
+    }
+})
+
+
 
 
 /** POST/:id/read - mark message as read:
@@ -27,4 +72,20 @@
  * Make sure that the only the intended recipient can mark as read.
  *
  **/
-
+router.post('/:id', async (req, res, next) => {
+    if(!req.user){
+        return next(new ExpressError("Invalid permissions", 401))
+    }
+    try {
+        const msg = await Message.get(req.params.id)
+        if (req.user == msg.to_user.username){
+            const resp = await Message.markRead(msg.id)
+            return res.json({message: resp})
+        }
+    } catch (e) {
+        return next(e)
+    }
+    // get token
+        // error if not valid
+    // mark as read with current timestamp
+})
