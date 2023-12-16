c92b8f5b8170fed50da90184e87cbae8819228f9
pun-intended <31040416+pun-intended@users.noreply.github.com>
2023-12-16 15:34:47 +0900

    Complete exercise

--- dsa-trees/binary-tree.js
+++ dsa-trees/binary-tree.js
@@ -1,3 +1,4 @@
+
 /** BinaryTreeNode: node for a general tree. */
 
 class BinaryTreeNode {
@@ -17,59 +18,137 @@ class BinaryTree {
    * the length of the shortest path from the root to a leaf. */
 
   minDepth() {
+    if(this.root === null) {return 0}
 
+    let minDepth, rightPath, leftPath;
+    function getPath(node, runningCount){
+      let count = runningCount
+      // cut off search if path longer than minDepth
+      if(minDepth && runningCount > minDepth) {return}
+      
+      if(node.right !== null){
+        rightPath = getPath(node.right, count+1)
+      } 
+      if(node.left !== null){
+        leftPath = getPath(node.left, count+1)
+      } 
+      if(node.right == null && node.left == null){
+
+        if (!minDepth || minDepth > count){
+          minDepth = count;
+          return;
+        }
+      }
+    }
+
+    getPath(this.root, 1)
+    return minDepth
   }
 
   /** maxDepth(): return the maximum depth of the tree -- that is,
    * the length of the longest path from the root to a leaf. */
 
   maxDepth() {
+    if(this.root === null) {return 0}
 
+    let maxDepth, rightPath, leftPath;
+    function getPath(node, runningCount){
+      let count = runningCount
+      
+      if(node.right !== null){
+        rightPath = getPath(node.right, count+1)
+      } 
+      if(node.left !== null){
+        leftPath = getPath(node.left, count+1)
+      } 
+      if(node.right == null && node.left == null){
+
+        if (!maxDepth || maxDepth < count){
+          maxDepth = count;
+          return;
+        }
+      }
+    }
+    
+    getPath(this.root, 1)
+    return maxDepth
   }
 
   /** maxSum(): return the maximum sum you can obtain by traveling along a path in the tree.
    * The path doesn't need to start at the root, but you can't visit a node more than once. */
 
   maxSum() {
+    if(this.root === null) {return 0}
+    let maxSum;
 
+    function getSum(node) {
+      let maxLeft = 0 
+      let maxRight = 0 
+      let throughSum = 0
+      if(node.left !== null) {maxLeft = getSum(node.left)};
+      if(node.right !== null) {maxRight = getSum(node.right)};
+      throughSum = node.val + maxRight + maxLeft
+      if(!maxSum || throughSum > maxSum) {maxSum = throughSum}
+      let max =  Math.max(node.val + maxLeft, node.val + maxRight)
+      return max
+    }
+
+    getSum(this.root)
+
+    return maxSum;
   }
 
   /** nextLarger(lowerBound): return the smallest value in the tree
    * which is larger than lowerBound. Return null if no such value exists. */
 
   nextLarger(lowerBound) {
+    if(this.root === null) {return null}
 
+    let path = [this.root];
+    let nextLarger = this.root > lowerBound ? this.root : null;
+    while(path.length){
+      let node = path.pop()
+      console.log(`${node.val} - ${nextLarger}`)
+
+      if(node.left) {path.push(node.left)}
+      if(node.right) {path.push(node.right)}
+      if (node.val > lowerBound){
+        if(nextLarger === null) {nextLarger = node.val}
+        nextLarger = Math.min(nextLarger, node.val)
+      }
+    }
+    return nextLarger;
   }
 
-  /** Further study!
-   * areCousins(node1, node2): determine whether two nodes are cousins
-   * (i.e. are at the same level but have different parents. ) */
+  // /** Further study!
+  //  * areCousins(node1, node2): determine whether two nodes are cousins
+  //  * (i.e. are at the same level but have different parents. ) */
 
-  areCousins(node1, node2) {
+  // areCousins(node1, node2) {
 
-  }
+  // }
 
-  /** Further study!
-   * serialize(tree): serialize the BinaryTree object tree into a string. */
+  // /** Further study!
+  //  * serialize(tree): serialize the BinaryTree object tree into a string. */
 
-  static serialize() {
+  // static serialize() {
 
-  }
+  // }
 
-  /** Further study!
-   * deserialize(stringTree): deserialize stringTree into a BinaryTree object. */
+  // /** Further study!
+  //  * deserialize(stringTree): deserialize stringTree into a BinaryTree object. */
 
-  static deserialize() {
+  // static deserialize() {
 
-  }
+  // }
 
-  /** Further study!
-   * lowestCommonAncestor(node1, node2): find the lowest common ancestor
-   * of two nodes in a binary tree. */
+  // /** Further study!
+  //  * lowestCommonAncestor(node1, node2): find the lowest common ancestor
+  //  * of two nodes in a binary tree. */
 
-  lowestCommonAncestor(node1, node2) {
+  // lowestCommonAncestor(node1, node2) {
     
-  }
+  // }
 }
 
 module.exports = { BinaryTree, BinaryTreeNode };
--- dsa-trees/tree.js
+++ dsa-trees/tree.js
@@ -15,20 +15,61 @@ class Tree {
   /** sumValues(): add up all of the values in the tree. */
 
   sumValues() {
-    
+    if(this.root === null) {return 0}
+
+    let sum = 0;
+    let checkStack = [this.root]
+
+    while (checkStack.length){
+      let current = checkStack.pop()
+      sum += current.val
+      for (let child of current.children){
+        checkStack.push(child)
+      }
+    }
+
+    return sum;
   }
 
   /** countEvens(): count all of the nodes in the tree with even values. */
 
   countEvens() {
+    if(this.root === null) {return 0}
 
+    let count = 0;
+    let checkStack = [this.root]
+
+    while (checkStack.length){
+      let current = checkStack.pop()
+      if (current.val % 2 === 0) {
+        count++
+      }
+      for (let child of current.children){
+        checkStack.push(child)
+      }
+    }
+    return count;
   }
 
   /** numGreater(lowerBound): return a count of the number of nodes
    * whose value is greater than lowerBound. */
 
   numGreater(lowerBound) {
+    if(this.root === null) {return 0}
 
+    let count = 0;
+    let checkStack = [this.root]
+
+    while (checkStack.length){
+      let current = checkStack.pop()
+      if (current.val > lowerBound) {
+        count++
+      }
+      for (let child of current.children){
+        checkStack.push(child)
+      }
+    }
+    return count;
   }
 }
 
