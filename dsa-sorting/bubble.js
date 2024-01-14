c8a86a5cd1dc2f01a00204108bb60b19dfe45f48
pun-intended <31040416+pun-intended@users.noreply.github.com>
2024-01-14 14:10:09 +0900

    Complete Sorting algorithms

--- dsa-sorting/bubble.js
+++ dsa-sorting/bubble.js
@@ -1,3 +1,15 @@
-function bubbleSort() {}
+function bubbleSort(array) {
+
+    for (let i = 0; i < array.length; i++){
+        for(let j = 0; j < array.length - i; j++){
+            if(array[j] > array[j+1]){
+                let temp = array[j]
+                array[j] = array[j+1]
+                array[j+1] = temp
+            }
+        }
+    }
+    return array
+}
 
 module.exports = bubbleSort;
--- dsa-sorting/merge.js
+++ dsa-sorting/merge.js
@@ -1,5 +1,37 @@
-function merge() {}
+function merge(array1, array2) {
+    let i = 0
+    let j = 0
+    const merged = []
+    while(i < array1.length && j < array2.length) {
+        if (array1[i] <= array2[j]){
+        merged.push(array1[i])
+        i++
+    } else {
+        merged.push(array2[j])
+        j++
+    }}
+    while(i < array1.length){
+        merged.push(array1[i])
+        i++
+    }
+    while(j < array2.length){
+        merged.push(array2[j])
+        j++
+    }
+    // console.log(merged)
+    return merged
+}
 
-function mergeSort() {}
+function mergeSort(arr) {
+    if(arr.length <= 1){
+        return arr
+    }
+    let mid = Math.floor(arr.length/2)
+    const arr1 = mergeSort(arr.slice(0, mid))
+    const arr2 = mergeSort(arr.slice(mid))
+    
+    return merge(arr1, arr2)
+
+}
 
 module.exports = { merge, mergeSort};
