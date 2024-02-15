66050fa5ff6fe581edc3d72e03dee13a2b0930c2
pun-intended <31040416+pun-intended@users.noreply.github.com>
2024-02-15 22:16:16 +0900

    start find rotated index exercise

--- prob-divide-conquer/find-rotated-index.js
+++ prob-divide-conquer/find-rotated-index.js
@@ -1,13 +1,22 @@
 function findRotatedIndex() {
- // start on outside
- // find middle
- // if compare to target and l/r idx
- // if middle > target:
- //     - if target > leftIdx -> move rightIdx to middle -1
- //     - if target < rightIdx -> move leftIdx middle +1
- // if middle < target
- //     - if target > leftIdx
- //     - if target < rightIdx
+ 
+}
+
+function findPivot(array){
+    // check if actually pivoted
+    if(array.length <= 1 || array[0] < array[array.length -1]) return 0;
+    // define start/end
+    let start = 0
+    let end = array.length - 1
+    // while start <= end, binary hop to pivot
+    while(start <= end){
+        let mid = start + Math.floor((end - start)/2)
+        if(array[mid] > array[mid + 1]) {
+            return mid + 1
+        } else if (array[start] <= array[mid])
+            start = mid + 1 
+        
+    }
 }
 
 module.exports = findRotatedIndex
