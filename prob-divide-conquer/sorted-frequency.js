5d11bfc345513ae4c7a3bfdc1373a843748f1dbb
pun-intended <31040416+pun-intended@users.noreply.github.com>
2024-02-15 22:15:54 +0900

    Complete sortedFrequency exercise

--- prob-divide-conquer/sorted-frequency.js
+++ prob-divide-conquer/sorted-frequency.js
@@ -1,13 +1,35 @@
-function sortedFrequency() {
-// find rightLimit, then leftLimit
-// rightLimit 
-// --> find middle, if middle < target lIndex = middle (repeat)
-// --> if middle > target 
-// - if middle > target, rightIdx = middle (repeat)
-// - if middle < target, leftIdx = middle (repeat)
-// - if middle == target ->check sides
+function sortedFrequency(array, num) {
+    let first = findFirst(array, num)
+    if (first === -1 ) return first
+    let last = findLast(array, num, first)
+    let count = last - first +1
+    return count
+}
 
+function findFirst(array, num, low=0, high=array.length - 1){
+    if(high >= low){
+        let mid = low + Math.floor((high - low)/2)
+        if((mid === 0 || array[mid - 1] < num) && array[mid] === num){
+            return mid
+        } else if (array[mid] < num) {
+            return findFirst(array, num, mid + 1, high)
+        }
+        return findFirst(array, num, low, mid - 1)
+    }
+    return - 1
+}
 
+function findLast(array, num, low=0, high=array.length - 1){
+    if(high >= low){
+        let mid = low + Math.floor((high - low)/2)
+        if((mid === array.length-1 || array[mid + 1] > num) && array[mid] === num){
+            return mid
+        } else if (array[mid] > num) {
+            return findLast(array, num, low, mid - 1)
+        }
+        return findLast(array, num, mid + 1, high)
+    }
+    return - 1
 }
 
 module.exports = sortedFrequency
