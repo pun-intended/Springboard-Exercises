d6afbf0b840f73261db09007aa1b904893fafc5d
pun-intended <31040416+pun-intended@users.noreply.github.com>
2024-02-19 16:31:35 +0900

    Complete guessing game

--- outcomes-js-tricky-parts/guessing-game.js
+++ outcomes-js-tricky-parts/guessing-game.js
@@ -1,5 +1,19 @@
 function guessingGame() {
-
+    let secretNum = Math.floor(Math.random()*100);
+    let count = 0;
+    let isOver = false;
+    return function guess(num) {
+        if (isOver) return "The game is over, you already won!"
+        count ++;
+        if(num === secretNum){
+            isOver = true;
+            return `You win! You found ${secretNum} in ${count} guesses.`;
+        } else if (num > secretNum) {
+            return `${num} is too high!`;
+        } else {
+            return `${num} is too low!`;
+        } 
+    }
 }
 
 module.exports = { guessingGame };
